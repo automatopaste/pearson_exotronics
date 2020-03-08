411b8fe5775151cc9828c87bc5f37774af747d60
Evan Phillips <raner9922@gmail.com>
2020-03-08 16:50:38 +0800

    initial

---
+++ jars/src/data/scripts/SPEModPlugin.java
@@ -0,0 +1,56 @@
+package data.scripts;
+
+import com.fs.starfarer.api.BaseModPlugin;
+import com.fs.starfarer.api.Global;
+import com.fs.starfarer.api.PluginPick;
+import com.fs.starfarer.api.campaign.CampaignPlugin;
+import com.fs.starfarer.api.combat.MissileAIPlugin;
+import com.fs.starfarer.api.combat.MissileAPI;
+import com.fs.starfarer.api.combat.ShipAIPlugin;
+import com.fs.starfarer.api.combat.ShipAPI;
+import com.fs.starfarer.api.fleet.FleetMemberAPI;
+//import data.scripts.ai.kiprad_droneCoronaDroneAI;
+//import data.scripts.weapons.ai.kiprad_eclipseAI;
+//import data.scripts.world.kiprad.KIPRADGen;
+import exerelin.campaign.SectorManager;
+
+public class SPEModPlugin extends BaseModPlugin {
+
+    public static final String ECLIPSE_MIRV_MISSILE_ID = "kiprad_eclipse_mirv";
+
+    public static final String DEUCES_DRONE_CORONA_ID = "kiprad_deuces";
+
+    @Override
+    public void onNewGame() {
+        boolean haveNexerelin = Global.getSettings().getModManager().isModEnabled("nexerelin");
+        if (!haveNexerelin || SectorManager.getCorvusMode()) {
+            //new KIPRADGen().generate(Global.getSector());
+        }
+    }
+
+    @Override
+    public void onApplicationLoad() throws ClassNotFoundException {
+
+        try {
+            Global.getSettings().getScriptClassLoader().loadClass("org.lazywizard.lazylib.ModUtils");
+        } catch (ClassNotFoundException ex) {
+            String message = System.lineSeparator()
+                    + System.lineSeparator() + "LazyLib is required to run at least one of the mods you have installed."
+                    + System.lineSeparator() + System.lineSeparator()
+                    + "You can download LazyLib at http://fractalsoftworks.com/forum/index.php?topic=5444"
+                    + System.lineSeparator();
+            throw new ClassNotFoundException(message);
+        }
+
+        try {
+            Global.getSettings().getScriptClassLoader().loadClass("data.scripts.util.MagicAnim");
+        } catch (ClassNotFoundException ex) {
+            String message = System.lineSeparator()
+                    + System.lineSeparator() + "MagicLib is required to run at least one of the mods you have installed."
+                    + System.lineSeparator() + System.lineSeparator()
+                    + "You can download MagicLib at http://fractalsoftworks.com/forum/index.php?topic=13718.0"
+                    + System.lineSeparator();
+            throw new ClassNotFoundException(message);
+        }
+    }
+}
