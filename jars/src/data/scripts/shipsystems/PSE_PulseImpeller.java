2b293257b5cb418e3060cba4017e97457c2d011c
Evan Phillips <raner9922@gmail.com>
2020-05-21 21:46:29 +0800

    updated impeller

--- jars/src/data/scripts/shipsystems/PSE_PulseImpeller.java
+++ jars/src/data/scripts/shipsystems/PSE_PulseImpeller.java
@@ -7,36 +7,39 @@ import com.fs.starfarer.api.combat.ShipEngineControllerAPI.ShipEngineAPI;
 import com.fs.starfarer.api.combat.ShipSystemAPI;
 import com.fs.starfarer.api.impl.combat.BaseShipSystemScript;
 import com.fs.starfarer.api.util.IntervalUtil;
+import org.lazywizard.lazylib.MathUtils;
 import org.lazywizard.lazylib.VectorUtils;
+import org.lazywizard.lazylib.combat.CombatUtils;
+import org.lwjgl.util.vector.Vector2f;
 
 import java.awt.*;
 import java.util.List;
 
-
+/*
+this was a bad idea
+ */
 public class PSE_PulseImpeller extends BaseShipSystemScript {
-    public static final float BOOST_SPEED = 300f;
+    public static final float BOOST_SPEED_CAP_MULT = 2f;
     public static final float FLAME_LENGTH_FRACTION = 2f;
     public static final float FLAME_GLOW_FRACTION = 2f;
-
-    public static final Color CONTRAIL_COLOUR = new Color (0xE2FF8B);
-    public static final Color JITTER_COLOUR = new Color (0x00FFB9);
+    public static final float JITTER_RANGE = 2f;
+    public static final Color CONTRAIL_COLOUR = new Color (0x63FFFC32, true);
+    public static final Color JITTER_COLOUR = new Color (0x5000FFB9, true);
 
     boolean started = false;
     boolean ended = false;
-    boolean boostForward = true;
 
     float boostFraction = 1f;
+    float boostLimit = 0f;
 
     IntervalUtil afterimageInterval = new IntervalUtil(0.1f, 0.15f);
     @Override
     public void apply(MutableShipStatsAPI stats, String id, State state, float effectLevel) {
-        float boostLimit = 0f;
+        float modlevel = effectLevel * 100f;
+
         ShipAPI ship = (ShipAPI) stats.getEntity();
         if (ship == null) {
             return;
-        }
-        if (!ended) {
-            boostLimit = ship.getVelocity().length() + BOOST_SPEED;
         }
 
         if (state.equals(State.IN)) {
@@ -46,6 +49,8 @@ public class PSE_PulseImpeller extends BaseShipSystemScript {
 
                 boostFraction = 1 - (ship.getNumFlameouts() / (float) engineList.size());
 
+                boostLimit = ship.getMaxSpeedWithoutBoost() * BOOST_SPEED_CAP_MULT;
+
                 started = true;
             }
 
@@ -53,17 +58,39 @@ public class PSE_PulseImpeller extends BaseShipSystemScript {
                 ship.getEngineController().extendFlame(engine, FLAME_LENGTH_FRACTION, 1f, FLAME_GLOW_FRACTION);
             }
 
-        } else if (state.equals(State.ACTIVE)) {
-            ship.setJitterUnder(this, JITTER_COLOUR, 10f * effectLevel, 6, 1f);
+            afterimageInterval.advance(0.1f);
+            if (afterimageInterval.intervalElapsed()) {
+                ship.addAfterimage(JITTER_COLOUR, MathUtils.getRandomNumberInRange(-1f * JITTER_RANGE, JITTER_RANGE), MathUtils.getRandomNumberInRange(-1f * JITTER_RANGE, JITTER_RANGE), ship.getVelocity().getX() * -0.5f, ship.getVelocity().getY() * -0.5f, 5f, 0.1f, 0f, 0.8f, true, false, false);
+            }
         } else if (state.equals(State.OUT)) {
             if (!ended) {
-                VectorUtils.resize(ship.getVelocity(), boostLimit * boostFraction * effectLevel);
+                if (VectorUtils.isZeroVector(ship.getVelocity())) {
+                    ship.getVelocity().y += 0.1f;
+                    VectorUtils.rotate(ship.getVelocity(), MathUtils.getShortestRotation(VectorUtils.getFacing(ship.getVelocity()), ship.getFacing()));
+                }
+
+                Vector2f facing = new Vector2f(0f, 1f);
+                VectorUtils.rotate(facing, MathUtils.getShortestRotation(VectorUtils.getFacing(facing), ship.getFacing()));
+                facing.normalise();
+                facing.scale(ship.getMass() * BOOST_SPEED_CAP_MULT);
+
+                Vector2f composite = new Vector2f(0f, 1f);
+                Vector2f.add(ship.getVelocity(), facing, composite);
+
+                CombatUtils.applyForce(ship, composite, ship.getMass() * BOOST_SPEED_CAP_MULT);
 
                 ended = true;
             }
+
             afterimageInterval.advance(0.01f);
             if (afterimageInterval.intervalElapsed()) {
-                ship.addAfterimage(CONTRAIL_COLOUR, 0f, 0f, ship.getVelocity().getX() * -0.5f, ship.getVelocity().getY() * -0.5f, 5f, 0.1f, 0f, 0.8f, true, false, false);
+                Color modColour = new Color(
+                        MathUtils.clamp(CONTRAIL_COLOUR.getRed() * modlevel, 1f, 255f) / 255f,
+                        MathUtils.clamp(CONTRAIL_COLOUR.getGreen() * modlevel, 1f, 255f) / 255f,
+                        MathUtils.clamp(CONTRAIL_COLOUR.getBlue() * modlevel, 1f, 255f) / 255f
+                    );
+
+                ship.addAfterimage(modColour, MathUtils.getRandomNumberInRange(-1f * JITTER_RANGE, JITTER_RANGE), MathUtils.getRandomNumberInRange(-1f * JITTER_RANGE, JITTER_RANGE), ship.getVelocity().getX() * -0.5f, ship.getVelocity().getY() * -0.5f, 5f, 0.1f, 0f, 0.8f, true, false, false);
             }
         }
     }
@@ -72,7 +99,6 @@ public class PSE_PulseImpeller extends BaseShipSystemScript {
     public void unapply(MutableShipStatsAPI stats, String id) {
         started = false;
         ended = false;
-        boostForward = true;
         boostFraction = 1f;
     }
 
