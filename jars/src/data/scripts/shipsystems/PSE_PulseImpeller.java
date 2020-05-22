95b7f1f4e90f65b71e4bd8185716678fa2476e08
Evan Phillips <raner9922@gmail.com>
2020-05-22 08:39:09 +0800

    updated impeller

--- jars/src/data/scripts/shipsystems/PSE_PulseImpeller.java
+++ jars/src/data/scripts/shipsystems/PSE_PulseImpeller.java
@@ -7,6 +7,7 @@ import com.fs.starfarer.api.combat.ShipEngineControllerAPI.ShipEngineAPI;
 import com.fs.starfarer.api.combat.ShipSystemAPI;
 import com.fs.starfarer.api.impl.combat.BaseShipSystemScript;
 import com.fs.starfarer.api.util.IntervalUtil;
+import data.scripts.util.PSE_BaseUtil;
 import org.lazywizard.lazylib.MathUtils;
 import org.lazywizard.lazylib.VectorUtils;
 import org.lazywizard.lazylib.combat.CombatUtils;
@@ -19,18 +20,20 @@ import java.util.List;
 this was a bad idea
  */
 public class PSE_PulseImpeller extends BaseShipSystemScript {
-    public static final float BOOST_SPEED_CAP_MULT = 2f;
+    public static final float BOOST_SPEED_MULT = 3f;
     public static final float FLAME_LENGTH_FRACTION = 2f;
     public static final float FLAME_GLOW_FRACTION = 2f;
     public static final float JITTER_RANGE = 2f;
-    public static final Color CONTRAIL_COLOUR = new Color (0x63FFFC32, true);
-    public static final Color JITTER_COLOUR = new Color (0x5000FFB9, true);
+    public static final Color CONTRAIL_COLOUR = new Color (0xFFFFFC32, true);
+    public static final Color JITTER_COLOUR = new Color (0xFF00FFB9, true);
+
 
     boolean started = false;
     boolean ended = false;
 
     float boostFraction = 1f;
     float boostLimit = 0f;
+    float initial = 0f;
 
     IntervalUtil afterimageInterval = new IntervalUtil(0.1f, 0.15f);
     @Override
@@ -42,6 +45,9 @@ public class PSE_PulseImpeller extends BaseShipSystemScript {
             return;
         }
 
+        ship.setJitterShields(false);
+
+        Vector2f composite = new Vector2f(0f, 1f);
         if (state.equals(State.IN)) {
             List<ShipEngineControllerAPI.ShipEngineAPI> engineList = ship.getEngineController().getShipEngines();
             if (!started) {
@@ -49,7 +55,8 @@ public class PSE_PulseImpeller extends BaseShipSystemScript {
 
                 boostFraction = 1 - (ship.getNumFlameouts() / (float) engineList.size());
 
-                boostLimit = ship.getMaxSpeedWithoutBoost() * BOOST_SPEED_CAP_MULT;
+                boostLimit = ship.getMaxSpeedWithoutBoost() * BOOST_SPEED_MULT;
+                initial = ship.getVelocity().length();
 
                 started = true;
             }
@@ -58,10 +65,9 @@ public class PSE_PulseImpeller extends BaseShipSystemScript {
                 ship.getEngineController().extendFlame(engine, FLAME_LENGTH_FRACTION, 1f, FLAME_GLOW_FRACTION);
             }
 
-            afterimageInterval.advance(0.1f);
-            if (afterimageInterval.intervalElapsed()) {
-                ship.addAfterimage(JITTER_COLOUR, MathUtils.getRandomNumberInRange(-1f * JITTER_RANGE, JITTER_RANGE), MathUtils.getRandomNumberInRange(-1f * JITTER_RANGE, JITTER_RANGE), ship.getVelocity().getX() * -0.5f, ship.getVelocity().getY() * -0.5f, 5f, 0.1f, 0f, 0.8f, true, false, false);
-            }
+            ship.setJitterUnder(this, JITTER_COLOUR, 10f * effectLevel, 8, 2f * effectLevel);
+        } else if (state.equals(State.ACTIVE)) {
+            ship.setJitterUnder(this, JITTER_COLOUR, 10f, 8, 3f);
         } else if (state.equals(State.OUT)) {
             if (!ended) {
                 if (VectorUtils.isZeroVector(ship.getVelocity())) {
@@ -72,12 +78,14 @@ public class PSE_PulseImpeller extends BaseShipSystemScript {
                 Vector2f facing = new Vector2f(0f, 1f);
                 VectorUtils.rotate(facing, MathUtils.getShortestRotation(VectorUtils.getFacing(facing), ship.getFacing()));
                 facing.normalise();
-                facing.scale(ship.getMass() * BOOST_SPEED_CAP_MULT);
+                facing.scale(ship.getMass() * BOOST_SPEED_MULT);
 
-                Vector2f composite = new Vector2f(0f, 1f);
-                Vector2f.add(ship.getVelocity(), facing, composite);
+                Vector2f velocity = ship.getVelocity();
+                velocity.scale(0.25f);
+                Vector2f.add(velocity, facing, composite);
 
-                CombatUtils.applyForce(ship, composite, ship.getMass() * BOOST_SPEED_CAP_MULT);
+                CombatUtils.applyForce(ship, composite, ship.getMass() * BOOST_SPEED_MULT);
+                VectorUtils.clampLength(ship.getVelocity(), boostLimit);
 
                 ended = true;
             }
@@ -90,7 +98,12 @@ public class PSE_PulseImpeller extends BaseShipSystemScript {
                         MathUtils.clamp(CONTRAIL_COLOUR.getBlue() * modlevel, 1f, 255f) / 255f
                     );
 
-                ship.addAfterimage(modColour, MathUtils.getRandomNumberInRange(-1f * JITTER_RANGE, JITTER_RANGE), MathUtils.getRandomNumberInRange(-1f * JITTER_RANGE, JITTER_RANGE), ship.getVelocity().getX() * -0.5f, ship.getVelocity().getY() * -0.5f, 5f, 0.1f, 0f, 0.8f, true, false, false);
+                PSE_BaseUtil.addJitterAfterimage(ship, modColour, JITTER_RANGE, -0.5f, 5f, 0.1f, 0f, 0.8f, true, false, false);
+            }
+            ship.setJitterUnder(this, JITTER_COLOUR, 10f * effectLevel, 8, 3f * effectLevel);
+
+            if (effectLevel <= 0.5f) {
+                VectorUtils.resize(ship.getVelocity(), initial + (effectLevel * boostLimit));
             }
         }
     }
@@ -100,6 +113,8 @@ public class PSE_PulseImpeller extends BaseShipSystemScript {
         started = false;
         ended = false;
         boostFraction = 1f;
+        boostLimit = 0f;
+        initial = 0f;
     }
 
     @Override
--- jars/src/data/scripts/util/PSE_BaseUtil.java
+++ jars/src/data/scripts/util/PSE_BaseUtil.java
@@ -10,6 +10,7 @@ import org.json.JSONObject;
 import org.lazywizard.lazylib.JSONUtils;
 import org.lazywizard.lazylib.MathUtils;
 
+import java.awt.*;
 import java.io.IOException;
 
 import static org.lazywizard.lazylib.combat.AIUtils.getEnemiesOnMap;
@@ -30,5 +31,9 @@ public class PSE_BaseUtil {
     public static JSONObject getDroneCoronaSpecJson() throws IOException, JSONException {
         final SettingsAPI settings = Global.getSettings();
         return settings.loadJSON(droneCoronaSpecJsonFilename);
+    }
+
+    public static void addJitterAfterimage(ShipAPI ship, Color color, float range, float velocityMult, float maxJitter, float in, float dur, float out, boolean additive, boolean combineWithSpriteColour, boolean aboveShip) {
+        ship.addAfterimage(color, MathUtils.getRandomNumberInRange(-1f * range, range), MathUtils.getRandomNumberInRange(-1f * range, range), ship.getVelocity().getX() * velocityMult, ship.getVelocity().getY() * velocityMult, maxJitter, in, dur, out, additive, combineWithSpriteColour, aboveShip);
     }
 }
